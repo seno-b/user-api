aef9b7e4d3ea0e31a913696cfb369453758d2509
seno-b <bangshinho@gmail.com>
2021-08-24 21:42:21 +0900

    feature: jwt security config 적용

--- src/main/java/com/senob/userapi/config/SecurityConfig.java
+++ src/main/java/com/senob/userapi/config/SecurityConfig.java
@@ -1,17 +1,52 @@
 package com.senob.userapi.config;
 
+import com.senob.userapi.jwt.JwtAccessDeniedHandler;
+import com.senob.userapi.jwt.JwtAuthenticationEntryPoint;
+import com.senob.userapi.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
-@EnableWebSecurity //기본적인 웹 시큐어 활성화
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final TokenProvider tokenProvider;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests() // http servlet request 접근제한 설정
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // token을 사용하는 방식이기 때문에 csrf를 disable합니다.
+                .csrf().disable()
+
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .accessDeniedHandler(jwtAccessDeniedHandler)
+
+                // enable h2-console
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                // 세션을 사용하지 않기 때문에 STATELESS로 설정
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 비활성화
+
+                .and()
+                .authorizeRequests()
                 .antMatchers("/api/hello").permitAll()
-                .anyRequest().authenticated(); // 나머진 모두 인증받아야 함
+                .antMatchers("/api/authenticate").permitAll()
+                .antMatchers("/api/signup").permitAll()
+
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new JwtSecurityConfig(tokenProvider));
     }
 }
